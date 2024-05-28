b9c9725e58f9d41b1eb587fe771d2b96ac97c6e6
sanjai570 <sanjaimarshy12456@gmail.com>
2024-05-28 17:38:18 +0530

    Update newstring.py

--- newstring.py
+++ newstring.py
@@ -1,1 +1,10 @@
 
+def create_new_string(input_str):
+    if len(input_str) < 3:
+        return "Input string should have at least 3 characters"
+    
+    middle_index = len(input_str) // 2
+    
+    new_str = input_str[0] + input_str[middle_index] + input_str[-1]
+    
+    return new_str
